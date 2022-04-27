4c8de6234bfc478ec21305ca20d271e6fb73e42f
adrianocarrieric~ <adrianocarrieri.dg@gmail.com>
2022-04-27 19:51:36 -0300

    actualizacion proycto

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3030, ()=>{
-    console.log('Servidor funcionando en el puerto http://localhost:3030');
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('Servidor funcionando en el puerto http://localhost:3000');
 });
 
 app.get('/', (req,res)=>{
